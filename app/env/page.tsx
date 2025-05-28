aa39deff81db35058f6969785fc1bfe22f6197ad
Toomtoomluvcat <kritsada.ba@kkumail.com>
2025-05-28 15:27:11 +0700

    add secret

--- app/env/page.tsx
+++ app/env/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 function page() {
   return (
     <div>{process.env.GOOGLE_CLIENT_SECRET?? ""}
-    <p>{ process.env.GOOGLE_CLIENT_ID}</p></div>
+    <p>{ process.env.GOOGLE_CLIENT_ID}</p>
+    <p>{ process.env.AUTH_SECRET}</p></div>
   )
 }
 

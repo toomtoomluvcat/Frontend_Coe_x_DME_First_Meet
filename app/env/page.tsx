adf9fadd9fd69607af93d73b99d297b3053b9cd9
Toomtoomluvcat <kritsada.ba@kkumail.com>
2025-05-28 15:20:39 +0700

    chage route

--- app/env/page.tsx
+++ app/env/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>{process.env.GOOGLE_CLIENT_SECRET?? ""}
-    <p>{ process.env.GOOGLE_CLIENT_SECRET}</p></div>
+    <p>{ process.env.GOOGLE_CLIENT_ID}</p></div>
   )
 }
 
